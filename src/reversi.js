1665bd49c02c76dc954bfd136bd99b3fdad2bec5
sturnbull95 <smt430@nyu.edu>
2017-02-08 19:41:56 -0500

    Update reversi.js

--- src/reversi.js
+++ src/reversi.js
@@ -90,7 +90,7 @@ function boardToString(board){
   for(var i = 0; i < size; i++){
     newArr[i] = alphabet[i] + '   ';
   }
-  newArr += newArr.join('');
+  arr += newArr.join('');
 
   var lines = repeat('+---',size);
   lines = lines.join('');
