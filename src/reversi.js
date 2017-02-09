f202697f8fc43abbd1021a9714df1738326ea037
sturnbull95 <smt430@nyu.edu>
2017-02-08 22:00:13 -0500

    Update reversi.js

--- src/reversi.js
+++ src/reversi.js
@@ -498,6 +498,7 @@ function getValidMoves(board, letter){
       var obj = indexToRowCol(myArr,i);
       var oneDArr = [];
       oneDArr.push(indexToRowCol(myArr,i).row,indexToRowCol(myArr,i).col);
+      console.log(indexToRowCol(myArr,i).row);
       if(isValidMove(myArr,letter,indexToRowCol(myArr,i).row,indexToRowCol(myArr,i).col)){
         twoDArr.push(oneDArr);
         console.log(twoDArr);
